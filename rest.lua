2e0f3824bd8b8600c72cf5c707c1bcc43a724c53
solace7289 <146842238+solace7289@users.noreply.github.com>
2025-10-12 23:56:32 +0700

    test lua

---
+++ rest.lua
@@ -0,0 +1,20 @@
+print("Hello world!")
+-- name = io.read("*l")
+-- print("Chào, " .. name .. "!")
+
+
+local a = 10
+local b = 3
+print("SUM:", a + b)
+print("String:", "Lua " .. "so easy!")
+
+
+
+
+
+
+
+
+
+
+
