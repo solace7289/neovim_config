8cd06c0be4aa6c0aa1d60288fb87556c86e1f0a1
solace7289 <146842238+solace7289@users.noreply.github.com>
2025-10-15 11:20:15 +0700

    modify init.lua, break smaller

---
+++ lua/config/settings.lua
@@ -0,0 +1,25 @@
+
+-- =========================
+-- Basic Neovim Settings
+-- =========================
+
+vim.g.mapleader = " "
+vim.wo.number = true
+vim.cmd("syntax on")
+vim.opt.clipboard = "unnamedplus"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+
+
+
+
+
+
+
+
+
+
+
+
